4586664ca1e6651ce0150473b66099b7890806e9
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-07-28 17:45:38 -0300

    iniciando repo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Banner from "./components/Banner"
+import Link from "./components/Link"
+
+function App () {
+  return (
+    <>
+      <Banner src="./BannerDesktop.png" alt="Banner"/>
+      <Link href="/" target="_blank">Link</Link>
+    </>
+  )
+
+}
+
+export default App
